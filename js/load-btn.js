5330b011255da504b57e46992401475fa435134a
Moch-Faisal <faishallonly@gmail.com>
2021-07-04 19:28:54 +0700

    update (7:28/4/jul/2021)

--- js/load-btn.js
+++ js/load-btn.js
@@ -1,11 +1,11 @@
 jQuery(document).ready(function ($) {
     $(function () {
         $("#collections .collection .collection-img").slice(0, 4).show();
-        $("body").on("click touchstart", "#collections .load-btn", function (e) {
+        $("body").on("click touchstart", "#collections .large-primary-btn", function (e) {
             e.preventDefault();
             $("#collections .collection .collection-img:hidden").slice(0, 4).fadeIn();
             if ($("#collections .collection .collection-img:hidden").length == 0) {
-                $("#collections .load-btn").css("visibility", "hidden");
+                $("#collections .large-primary-btn").css("visibility", "hidden");
             }
             // $("html,body").animate(
             //     {
