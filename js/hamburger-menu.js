92f81bc8ee59da74ee26d875b7560eebc588cb2c
Moch-Faisal <faishallonly@gmail.com>
2021-07-05 20:08:30 +0700

    responsive (8.07/5/jul/2021)pm

---
+++ js/hamburger-menu.js
@@ -0,0 +1,8 @@
+function hamburgerNav() {
+    var x = document.getElementById("navbar");
+    if (x.className === "nav") {
+        x.className += " responsive";
+    } else {
+        x.className = "nav";
+    }
+}
